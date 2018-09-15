2c294d42451432aafa829a1b9f59a185fe2f7014
andrew <perepelkin.work@gmail.com>
2018-09-15 14:01:02 +0300

    uses other logger

--- src/main/java/ru/aperepelkin/revolut/account/Application.java
+++ src/main/java/ru/aperepelkin/revolut/account/Application.java
@@ -2,9 +2,13 @@ package ru.aperepelkin.revolut.account;
 
 import ru.aperepelkin.revolut.account.bootstrap.Bootstrap;
 
+import java.io.IOException;
+import java.util.logging.LogManager;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        LogManager.getLogManager().readConfiguration(ClassLoader.getSystemResourceAsStream("log.properties"));
         Bootstrap bootstrap = Bootstrap.builder()
                 .daemon()
                 .httpServer().port(8080)
